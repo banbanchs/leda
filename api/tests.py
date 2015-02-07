5eac153f566af27263c430c1e5516dba8d2a3646
banbanchs <memory.silentvoyage@gmail.com>
2015-02-07 17:54:42 +0800

    Add api test

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,31 @@
-from django.test import TestCase
+# coding=utf-8
 
-# Create your tests here.
+from django.core.management import call_command
+from django.core.urlresolvers import reverse
+from rest_framework.test import APIClient, APITestCase
+from rest_framework import status
+
+
+class ApiTest(APITestCase):
+    def setUp(self):
+        """Setup test http client"""
+        self.client = APIClient()
+        self.city = {'city': 'Guangzhou'}
+        # load fixtures
+        call_command('loaddata', 'dumpdata.json', verbosity=0)
+
+    def test_air_condition(self):
+        """Test hourly pm2.5 api"""
+        url = reverse('api:pm25-list', kwargs=self.city)
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('Guangzhou', response.content)
+
+    def test_air_average(self):
+        """Test pm2.5 average api"""
+        url = reverse('api:pm25-avg-list', kwargs=self.city)
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('Guangzhou', response.content)
+        self.assertIn('from_time', response.content)
+        self.assertIn('to_time', response.content)
--- leda/urls.py
+++ leda/urls.py
@@ -6,5 +6,5 @@ from api.urls import api_router
 
 urlpatterns = [
     url(r'^$', include('home.urls', namespace='home')),
-    url(r'^api/', include(api_router.urls)),
+    url(r'^api/', include(api_router.urls, namespace='api')),
 ]
