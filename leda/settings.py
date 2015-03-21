bb1be8987cbce99e44fdaabbf564de8813b64c41
banbanchs <memory.silentvoyage@gmail.com>
2015-03-21 12:41:50 +0800

    Add cache

--- leda/settings.py
+++ leda/settings.py
@@ -148,6 +148,22 @@ STATICFILES_DIRS = (
 )
 
 
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/0",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+            "SOCKET_TIMEOUT": 5,
+        }
+    }
+}
+
+
+SESSION_ENGINE = "django.contrib.sessions.backends.cache"
+SESSION_CACHE_ALIAS = "default"
+
+
 # Twitter key
 
 CLIENT_KEY = ''
